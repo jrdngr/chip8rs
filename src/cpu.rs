6d77d7e15e92c0089a2f35d5de44b8a80eddc1f9
Jordan Grace <jordan.t.grace@gmail.com>
2018-07-09 21:28:14 -0400

    Implemented some opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use super::opcode::{ OpCode };
+
 pub struct Cpu {
     program_counter: u16,
     stack_pointer: u8,
@@ -20,6 +22,137 @@ impl Cpu {
             data_registers: [0; 16],
             stack: [0; 16],
             ram: [0; 4096],
+        }
+    }
+
+    pub fn process_opcode(&mut self, opcode: OpCode) {
+        match opcode {
+            OpCode::ExecuteMachineSubroutine(nnn) => { /* Not implemented */ },
+            OpCode::ClearScreen => {   
+                
+            },
+            OpCode::ReturnFromSubroutine => {   
+
+            },
+            OpCode::JumpTo(nnn) => {   
+
+            },
+            OpCode::ExecuteSubroutine(nnn) => {   
+
+            },
+            OpCode::SkipIfEqualValue(x, nn) => {   
+
+            },
+            OpCode::SkipIfNotEqualValue(x, nn) => {   
+
+            },
+            OpCode::SkipIfEqualRegister(x, y) => {   
+
+            },
+            OpCode::StoreValue(x, nn) => { 
+                self.data_registers[x as usize] = nn; 
+            },
+            OpCode::AddValue(x, nn) => { 
+                self.data_registers[x as usize] += nn; 
+            },
+            OpCode::StoreRegister(x, y) => { 
+                self.data_registers[x as usize] = self.data_registers[y as usize]; 
+            },
+            OpCode::Or(x, y) => {  
+                let vx = self.data_registers[x as usize];
+                let vy = self.data_registers[y as usize];
+                self.data_registers[x as usize] = vx | vy;
+            },
+            OpCode::And(x, y) => {
+                let vx = self.data_registers[x as usize];
+                let vy = self.data_registers[y as usize];
+                self.data_registers[x as usize] = vx & vy;
+            },
+            OpCode::Xor(x, y) => {
+                let vx = self.data_registers[x as usize];
+                let vy = self.data_registers[y as usize];
+                self.data_registers[x as usize] = vx ^ vy;
+            },
+            OpCode::AddRegister(x, y) => { 
+                let vx = self.data_registers[x as usize];
+                let vy = self.data_registers[y as usize];
+                let (sum, overflowed) = vx.overflowing_add(vy);
+                self.data_registers[x as usize] = sum;
+                self.data_registers[15] = if overflowed { 1 } else { 0 };
+            },
+            OpCode::SubtractRegister(x, y) => { 
+                let vx = self.data_registers[x as usize];
+                let vy = self.data_registers[y as usize];
+                let (sum, overflowed) = vx.overflowing_sub(vy);
+                self.data_registers[x as usize] = sum;
+                self.data_registers[15] = if overflowed { 0 } else { 1 };
+            },
+            OpCode::ShiftRight(x, y) => {
+                let vy = self.data_registers[y as usize];
+                self.data_registers[15] = vy & 1;
+                self.data_registers[x as usize] = vy >> 1;
+            },
+            OpCode::SubtractRegisterReverse(x, y) => {
+                let vx = self.data_registers[x as usize];
+                let vy = self.data_registers[y as usize];
+                let (sum, overflowed) = vy.overflowing_sub(vx);
+                self.data_registers[x as usize] = sum;
+                self.data_registers[15] = if overflowed { 0 } else { 1 };
+            },
+            OpCode::ShiftLeft(x, y) => {
+                let vy = self.data_registers[y as usize];
+                self.data_registers[15] = (vy >> 7) & 1;
+                self.data_registers[x as usize] = vy << 1;
+
+            },
+            OpCode::SkipIfNotEqualRegister(x, y) => {   
+
+            },
+            OpCode::StoreInI(nnn) => { 
+                self.i_register = nnn 
+            },
+            OpCode::JumpWithOffset(nnn) => {   
+
+            },
+            OpCode::SetToRandom(x, nn) => {   
+
+            },
+            OpCode::DrawSprite(x, y, n) => {   
+
+            },
+            OpCode::SkipIfKeyPressed(x) => {   
+
+            },
+            OpCode::SkipIfKeyNotPressed(x) => {   
+
+            },
+            OpCode::StoreDelayTimer(x) => { 
+                self.data_registers[x as usize] = self.delay_timer; 
+            },
+            OpCode::WaitAndStoreKey(x) => {   
+
+            },
+            OpCode::SetDelayTimer(x) => { 
+                self.delay_timer = self.data_registers[x as usize]; 
+            },
+            OpCode::SetSoundTimer(x) => { 
+                self.sound_timer = self.data_registers[x as usize]; 
+            },
+            OpCode::AddToRegisterI(x) => { 
+                self.i_register += self.data_registers[x as usize] as u16; 
+            },
+            OpCode::SetIToSprite(x) => { 
+
+            },
+            OpCode::StoreDecimal(x) => {   
+
+            },
+            OpCode::StoreRegisters(x) => {   
+
+            },
+            OpCode::FillRegisters(x) => {   
+
+            },
         }
     }
 }
