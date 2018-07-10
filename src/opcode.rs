6f59ea49e0cab3ce5072ece42d02cad45152c340
Jordan Grace <jordan.t.grace@gmail.com>
2018-07-09 20:10:30 -0400

    Finished opcode parsing

--- src/opcode.rs
+++ src/opcode.rs
@@ -52,10 +52,23 @@ impl From<u16> for OpCode {
             0x2 => OpCode::ExecuteSubroutine(get_last_12_bits(value)),
             0x3 => OpCode::SkipIfEqualValue(x, get_last_byte(value)),
             0x4 => OpCode::SkipIfNotEqualValue(x, get_last_byte(value)),
-            0x5 => OpCode::SkipIfEqualRegister(x, y),
+            0x5 => if n == 0 {
+                OpCode::SkipIfEqualRegister(x, y)
+            } else {
+                panic!("{} is not a valid opcode", value)
+            },
             0x6 => OpCode::StoreValue(x, get_last_byte(value)),
             0x7 => OpCode::AddValue(x, get_last_byte(value)),
             0x8 => match n {
+                0x0 => OpCode::StoreRegister(x, y),
+                0x1 => OpCode::Or(x, y),
+                0x2 => OpCode::And(x, y),
+                0x3 => OpCode::Xor(x, y),
+                0x4 => OpCode::AddRegister(x, y),
+                0x5 => OpCode::SubtractRegister(x, y),
+                0x6 => OpCode::ShiftRight(x, y),
+                0x7 => OpCode::SubtractRegisterReverse(x, y),
+                0xE => OpCode::ShiftLeft(x, y),
                 _ => panic!("{} is not a valid opcode", value),
             },
             0x9 => OpCode::SkipIfNotEqualRegister(x, y),
@@ -63,6 +76,23 @@ impl From<u16> for OpCode {
             0xB => OpCode::JumpWithOffset(get_last_12_bits(value)),
             0xC => OpCode::SetToRandom(x, get_last_byte(value)),
             0xD => OpCode::DrawSprite(x, y, n),
+            0xE => match get_last_byte(value) {
+                0x9E => OpCode::SkipIfKeyPressed(x),
+                0xA1 => OpCode::SkipIfKeyNotPressed(x),
+                _ => panic!("{} is not a valid opcode", value),
+            },
+            0xF => match get_last_byte(value) {
+                0x07 => OpCode::StoreDelayTimer(x),
+                0x0A => OpCode::WaitAndStoreKey(x),
+                0x15 => OpCode::SetDelayTimer(x),
+                0x18 => OpCode::SetSoundTimer(x),
+                0x1E => OpCode::AddToRegisterI(x),
+                0x29 => OpCode::SetIToSprite(x),
+                0x33 => OpCode::StoreDecimal(x),
+                0x55 => OpCode::StoreRegisters(x),
+                0x65 => OpCode::FillRegisters(x),
+                _ => panic!("{} is not a valid opcode", value),
+            },
             _ => panic!("{} is not a valid opcode", value),
         }
     }
@@ -89,16 +119,50 @@ fn get_last_12_bits(value: u16) -> u16 {
 mod tests {
     use super::*;
 
-    #[test]
+   #[test]
     fn test_opcodes() {
+        assert_eq!(OpCode::from(0x01B3), OpCode::ExecuteMachineSubroutine(0x1B3));
         assert_eq!(OpCode::from(0x00E0), OpCode::ClearScreen);
         assert_eq!(OpCode::from(0x00EE), OpCode::ReturnFromSubroutine);
+        assert_eq!(OpCode::from(0x1123), OpCode::JumpTo(0x123));
+        assert_eq!(OpCode::from(0x23B2), OpCode::ExecuteSubroutine(0x3B2));
+        assert_eq!(OpCode::from(0x3123), OpCode::SkipIfEqualValue(1, 0x23));
+        assert_eq!(OpCode::from(0x4123), OpCode::SkipIfNotEqualValue(1, 0x23));
+        assert_eq!(OpCode::from(0x5120), OpCode::SkipIfEqualRegister(1, 2));
+        assert_eq!(OpCode::from(0x6123), OpCode::StoreValue(1, 0x23));
+        assert_eq!(OpCode::from(0x7123), OpCode::AddValue(1, 0x23));
+        assert_eq!(OpCode::from(0x8120), OpCode::StoreRegister(1, 2));
+        assert_eq!(OpCode::from(0x8121), OpCode::Or(1, 2));
+        assert_eq!(OpCode::from(0x8122), OpCode::And(1, 2));
+        assert_eq!(OpCode::from(0x8123), OpCode::Xor(1, 2));
+        assert_eq!(OpCode::from(0x8124), OpCode::AddRegister(1, 2));
+        assert_eq!(OpCode::from(0x8125), OpCode::SubtractRegister(1, 2));
+        assert_eq!(OpCode::from(0x8126), OpCode::ShiftRight(1, 2));
+        assert_eq!(OpCode::from(0x8127), OpCode::SubtractRegisterReverse(1, 2));
+        assert_eq!(OpCode::from(0x812E), OpCode::ShiftLeft(1, 2));
+        assert_eq!(OpCode::from(0x9120), OpCode::SkipIfNotEqualRegister(1, 2));
+        assert_eq!(OpCode::from(0xA123), OpCode::StoreInI(0x123));
+        assert_eq!(OpCode::from(0xB123), OpCode::JumpWithOffset(0x123));
+        assert_eq!(OpCode::from(0xC123), OpCode::SetToRandom(1, 0x23));
+        assert_eq!(OpCode::from(0xD123), OpCode::DrawSprite(1, 2, 3));
+        assert_eq!(OpCode::from(0xE19E), OpCode::SkipIfKeyPressed(1));
+        assert_eq!(OpCode::from(0xE1A1), OpCode::SkipIfKeyNotPressed(1));
+        assert_eq!(OpCode::from(0xF107), OpCode::StoreDelayTimer(1));
+        assert_eq!(OpCode::from(0xF10A), OpCode::WaitAndStoreKey(1));
+        assert_eq!(OpCode::from(0xF115), OpCode::SetDelayTimer(1));
+        assert_eq!(OpCode::from(0xF118), OpCode::SetSoundTimer(1));
+        assert_eq!(OpCode::from(0xF11E), OpCode::AddToRegisterI(1));
+        assert_eq!(OpCode::from(0xF129), OpCode::SetIToSprite(1));
+        assert_eq!(OpCode::from(0xF133), OpCode::StoreDecimal(1));
+        assert_eq!(OpCode::from(0xF155), OpCode::StoreRegisters(1));
+        assert_eq!(OpCode::from(0xF165), OpCode::FillRegisters(1));
     }
 
     #[test]
     #[should_panic]
     fn test_invalid_opcodes() {
         let op = OpCode::from(0x5EE1);
+        println!("{:?}", op);
     }
 
     #[test]
