7b1d6467f649aa313c9b46316c3575488a04fa14
Jordan Grace <jordan.t.grace@gmail.com>
2018-07-31 20:13:07 -0400

    display is more efficient now

--- web/src/display.ts
+++ web/src/display.ts
@@ -2,62 +2,62 @@ import * as PIXI from 'pixi.js';
 
 const PIXEL_SIZE = 15;
 const PIXEL_ON_COLOR = 0x000000;
-const PIXEL_OFF_COLOR = 0xFFFFFF;
 
 export class Display {
     private readonly width: number;
     private readonly height: number;
-    private readonly pixels: boolean[] = [];
+    private readonly pixels: PIXI.Graphics[] = [];
     
     private readonly app: PIXI.Application;
 
     constructor(width: number, height: number)
     {
-        this.width = PIXEL_SIZE * width;
-        this.height = PIXEL_SIZE * height;
+        this.width = width;
+        this.height = height;
 
-        this.app = new PIXI.Application(
-            { backgroundColor: 0xFFFFFF, }
-        );
+        this.app = new PIXI.Application({  
+                width: width * PIXEL_SIZE,
+                height: height * PIXEL_SIZE,
+                backgroundColor: 0xFFFFFF, 
+            });
         const screenDiv = document.getElementById("screen");
         screenDiv.appendChild(this.app.view);
 
         for (let i = 0, max = width * height; i < max; i++) {
-            this.pixels.push(false);
+            const {x, y} = this.getCoordinates(i);
+            const pixel = this.drawPixel(x, y)
+            this.pixels.push(pixel);
+            this.app.stage.addChild(pixel);
         }
     }
 
     public setPixel(x: number, y: number) {
         const index = this.getIndex(x, y);
-        const current = this.pixels[index];
-        if (!current) {
-            this.pixels[index] = true;
-        } else {
-            this.pixels[index] = false;
-        }
-        
-        this.drawPixel(x, y, this.pixels[index]);
+        this.pixels[index].visible = !this.pixels[index].visible;
     }
 
     public clear() {
-        for (let row = 0; row < this.width; row++) {
-            for (let col = 0; col < this.height; col++) {
-                let index = this.getIndex(row, col);
-                this.pixels[index] = false;
-                this.drawPixel(row, col, false);
-            }
-        }
+        this.pixels.forEach(pixel => {
+            pixel.fillAlpha = 0;
+        });
     }
 
-    private drawPixel(x: number, y: number, isOn: boolean) {
+    private drawPixel(x: number, y: number): PIXI.Graphics {
         let rectangle = new PIXI.Graphics();
-        rectangle.beginFill(isOn ? PIXEL_ON_COLOR : PIXEL_OFF_COLOR);
+        rectangle.beginFill(0x000000);
         rectangle.drawRect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
         rectangle.endFill();
-        this.app.stage.addChild(rectangle);
+        rectangle.visible = false;
+        return rectangle;
     }
 
     private getIndex(x: number, y: number): number {
         return (y * this.width) + x;
+    }
+
+    private getCoordinates(index: number): { x: number, y: number } {
+        const x = index % this.width;
+        const y = Math.floor(index / this.width);
+        return { x, y };
     }
 }
